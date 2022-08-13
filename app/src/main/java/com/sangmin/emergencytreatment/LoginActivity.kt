7c3b6fdd8a3ac292c54a035398e459cb83c5f289
박 상 민 <dhwkffks7@naver.com>
2022-08-13 16:28:45 +0900

    로그인 화면 xml 구성 / 회원가입 구성 예정

---
+++ app/src/main/java/com/sangmin/emergencytreatment/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.sangmin.emergencytreatment
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+
+
+}
